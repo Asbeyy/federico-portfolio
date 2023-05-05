c7b4abcdc47437984a67bb0085a64b59802c28c0
Asbeyy <you@example.com>
2023-05-05 04:55:08 +0200

    fix aspect ratio scene2

--- second3d.js
+++ second3d.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { WebGLRenderer } from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,screen.width / screen.height , 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 1000)
 
 
 //* Cube
